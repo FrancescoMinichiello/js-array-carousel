5d3498a0a5c21cac88efd3e2013e31aef1deaac6
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-20 16:47:15 +0200

    Add img with JS

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Fase di preparazione
 
+const sources = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp']
+
 //Recupero gli elementi dal dom
 
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
+const carouselGallery = document.querySelector('.gallery');
+//genero le immagini da JS
+
+let imgs = '';
+
+for (let i = 0; i < sources.length; i++) {
+    const currentElement = sources[i];
+    imgs += `<img alt="landscape-${i + 1}" src="${sources[i]}">`;
+}
+
+carouselGallery.innerHTML = imgs;
+
 const images = document.querySelectorAll('#carousel img');
+
 
 //Rendiamo visibile la prima immagine
 let currentActiveIndex = 0;
@@ -19,6 +39,13 @@ nextButton.addEventListener('click', function () {
     images[currentActiveIndex].classList.remove('active');
     //incremento l'indice
     currentActiveIndex++;
+
+    //controlliamo se siamo fuori array
+
+    if (currentActiveIndex === images.length) {
+        currentActiveIndex = 0;
+    }
+
     //Metto la classe active all'immagine successiva
     images[currentActiveIndex].classList.add('active');
 })
@@ -32,6 +59,10 @@ prevButton.addEventListener('click', function () {
 
     //Decremento l'indice
     currentActiveIndex--;
+
+    if (currentActiveIndex < 0) {
+        currentActiveIndex = images.length - 1;
+    }
 
     //Metto la classe active all'immagine successiva
     images[currentActiveIndex].classList.add('active');
