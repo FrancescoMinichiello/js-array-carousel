14c0ffa0e453ad74bffd6178500f2c76f6ab8da3
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-20 15:39:30 +0200

    Add eventlistener button prev & next

--- script.js
+++ script.js
@@ -2,5 +2,37 @@
 
 //Recupero gli elementi dal dom
 
-const prevButton = document.getElementById('prev')
-const nextButton = document.getElementById('next')
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const images = document.querySelectorAll('#carousel img');
+
+//Rendiamo visibile la prima immagine
+let currentActiveIndex = 0;
+images[currentActiveIndex].classList.add('active');
+
+
+// Eventi dinamici
+
+//Reagiamo al next
+nextButton.addEventListener('click', function () {
+    //togliere la classe active all'immagine attualmente attiva
+    images[currentActiveIndex].classList.remove('active');
+    //incremento l'indice
+    currentActiveIndex++;
+    //Metto la classe active all'immagine successiva
+    images[currentActiveIndex].classList.add('active');
+})
+
+
+
+//Reagiamo al prev
+prevButton.addEventListener('click', function () {
+    //togliere la classe active all'immagine attualmente attiva
+    images[currentActiveIndex].classList.remove('active');
+
+    //Decremento l'indice
+    currentActiveIndex--;
+
+    //Metto la classe active all'immagine successiva
+    images[currentActiveIndex].classList.add('active');
+})
